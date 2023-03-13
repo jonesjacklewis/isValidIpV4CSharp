a3ceb64dea2a0ff85ffee3abdb187a9273c76b60
jonesjacklewis <jones.jack.lewis@gmail.com>
2023-03-13 22:11:34 +0000

    First Commit

---
+++ IpV4Tests/UnitTest1.cs
@@ -0,0 +1,77 @@
+﻿namespace IpV4Tests;
+
+using IsValidIpV4;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    /*
+     * All 0s --> True
+     * All 255 --> True
+     * Valid IP Address --> True
+     * Length < 4 --> False
+     * Length > 4 --> False
+     * Alphanumeric --> False
+     * Alphabetic --> False
+     * Contains Padding --> False
+     * Octet Starts With Leading 0 --> False
+     */
+
+    [Test]
+    public void Given_FourOctetsOfAllZero_ShouldBeTrue()
+    {
+        Assert.That(Program.IsValidIpV4("0.0.0.0"), Is.True);
+    }
+
+    [Test]
+    public void Given_FourOctetsOfAll255_ShouldBeTrue()
+    {
+        Assert.That(Program.IsValidIpV4("255.255.255.255"), Is.True);
+    }
+
+    [Test]
+    public void Given_ValidIpV4_ShouldBeTrue()
+    {
+        Assert.That(Program.IsValidIpV4("128.55.25.255"), Is.True);
+    }
+
+    [Test]
+    public void Given_LessThanFourOctets_ShouldBeFalse()
+    {
+        Assert.That(Program.IsValidIpV4("55.25.255"), Is.False);
+    }
+
+    [Test]
+    public void Given_MoreThanFourOctets_ShouldBeFalse()
+    {
+        Assert.That(Program.IsValidIpV4("55.25.255.127.128"), Is.False);
+    }
+
+    [Test]
+    public void Given_AlphanumericOctets_ShouldBeFalse()
+    {
+        Assert.That(Program.IsValidIpV4("55.25.255.abc"), Is.False);
+    }
+
+    [Test]
+    public void Given_AlphabeticOctets_ShouldBeFalse()
+    {
+        Assert.That(Program.IsValidIpV4("abc.def.ghi.jkl"), Is.False);
+    }
+
+    [Test]
+    public void Given_OctetContainsPadding_ShouldBeFalse()
+    {
+        Assert.That(Program.IsValidIpV4("128.0.0. 0"), Is.False);
+    }
+
+    [Test]
+    public void Given_OctetHadLeadingZero_ShouldBeFalse()
+    {
+        Assert.That(Program.IsValidIpV4("128.0.0.015"), Is.False);
+    }
+}
---
+++ IsValidIpV4/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace IsValidIpV4
+{ 
+    using System.Text.RegularExpressions;
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] validIps = { "123.45.67.89", "1.2.3.4" };
+            string[] invalidIps = { "123.456.78.90", "1.2.3", "1.2.3.4.5", "123.045.067.089" };
+            string message;
+
+            foreach (string ip in validIps)
+            {
+                message = IsValidIpV4(ip) ? "is valid" : "is not valid";
+                Console.WriteLine($"{ip} - {message}");
+            }
+
+            foreach (string ip in invalidIps)
+            {
+                message = IsValidIpV4(ip) ? "is valid" : "is not valid";
+                Console.WriteLine($"{ip} - {message}");
+            }
+        }
+
+        public static bool IsValidIpV4(string ip)
+        {
+            string[] octets = GetOctets(ip);
+
+            if (!(octets.Length == 4))
+            {
+                return false;
+            }
+
+            foreach (string octet in octets)
+            {
+                if (OctetStartsWithLeadingZero(octet))
+                {
+                    return false;
+                }
+
+                if (!OctetBetween0And255(octet))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static string[] GetOctets(string ip)
+        {
+            return ip.Split('.');
+        }
+
+        public static bool OctetStartsWithLeadingZero(string octet)
+        {
+            return octet[0] == '0' && octet.Length != 1;
+        }
+
+        public static bool OctetBetween0And255(string octet)
+        {
+            int intOctet;
+
+            if (!int.TryParse(octet, out intOctet) || !octet.All(char.IsNumber))
+            {
+                return false;
+            }
+
+            return 0 <= intOctet && intOctet <= 255;
+        }
+
+    }
+}
